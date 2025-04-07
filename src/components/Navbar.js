1b003af14cc1bcfc3a00f31dbdd05cad06660fbe
Lakshya Porwal <89459982+lakshya2601@users.noreply.github.com>
2025-04-07 22:56:51 +0530

    refactor(Navbar): comment out Resume link for temporary removal
    The Resume link in the Navbar has been commented out to temporarily remove it from the UI. This change is made to streamline the navigation options while the resume section is being updated.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -88,7 +88,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+            {/* <Nav.Item>
               <Nav.Link
                 as={Link}
                 to="/resume"
@@ -96,7 +96,7 @@ function NavBar() {
               >
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
 
             <Nav.Item>
               <Nav.Link
